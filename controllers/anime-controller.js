d2c3b9a4b93629a9665ddfe21cf36f65bb269ce5
brokiem <milyhqvideo@gmail.com>
2023-01-15 00:39:38 +0800

    don't import everything from mal-scraper

--- controllers/anime-controller.js
+++ controllers/anime-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as mal from 'mal-scraper';
+import {getInfoFromName, getRecommendationsList} from 'mal-scraper';
 
 export const getAnimeDetails = async (req, res) => {
     const { title } = req.params;
@@ -15,14 +15,14 @@ export const getAnimeDetails = async (req, res) => {
         let promises = [];
 
         for (let i = 0; i < titles.length; i++) {
-            promises.push(mal.getInfoFromName(titles[i]));
+            promises.push(getInfoFromName(titles[i]));
         }
 
         Promise.all(promises).then((values) => {
             res.status(200).json({"success": true, "message": values});
         });
     } else {
-        mal.getInfoFromName(req.params.title)
+        getInfoFromName(req.params.title)
             .then((data) => {
                 res.status(200).json({"success": true, "message": data});
             })
@@ -33,7 +33,7 @@ export const getAnimeDetails = async (req, res) => {
 }
 
 export const getAnimeRecommendations = async (req, res) => {
-    mal.getRecommendationsList('anything')
+    getRecommendationsList('anything')
         .then((data) => {
             res.status(200).json({"success": true, "message": data});
         })
@@ -45,7 +45,7 @@ export const getAnimeRecommendations = async (req, res) => {
 export const getAnimeRecommendationsByName = async (req, res) => {
     const name = req.params.name;
 
-    mal.getRecommendationsList(name)
+    getRecommendationsList(name)
         .then((data) => {
             res.status(200).json({"success": true, "message": data});
         })
